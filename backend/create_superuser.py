3c173b43e76c163fb7c3289ce3293de67bb94be7
Nikita Arutiunov <nickaroot@nickaroot.me>
2024-02-21 06:49:26 +0300

    Django Superuser added

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -13,6 +13,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 try:
-    User.objects.create_superuser('test', 'test@example.com', 'test')
+    User.objects.create_superuser(os.getenv('SUPERADMIN_USERNAME'), os.getenv('SUPERADMIN_EMAIL'), os.getenv('SUPERADMIN_PASSWORD'))
 except Exception:
     pass
